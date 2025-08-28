8394283d8be6add1a822058767bdf23ff3be7cfd
millanda29 <maikolllanda@gmail.com>
2025-08-27 20:36:46 -0500

    feat(models): ✨ create models for Actor, Pelicula, and Elenco

---
+++ server/src/models/actorModel.js
@@ -0,0 +1,7 @@
+export default class Actor {
+  constructor({ id_actor, nombre, status }) {
+    this.id_actor = id_actor;
+    this.nombre = nombre;
+    this.status = status;
+  }
+}
---
+++ server/src/models/elencoModel.js
@@ -0,0 +1,9 @@
+export default class Elenco {
+  constructor({ id_elenco, id_pelicula, id_actor, personaje, status }) {
+    this.id_elenco = id_elenco;
+    this.id_pelicula = id_pelicula;
+    this.id_actor = id_actor;
+    this.personaje = personaje;
+    this.status = status;
+  }
+}
